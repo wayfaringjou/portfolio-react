9006849cba81d75afd331238d231592b177103b2
Joel Del Cueto <wayfaringjou@gmail.com>
2021-04-08 22:26:47 -0400

    Add root and projectCard components

---
+++ src/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectCard = () => (
+  <article>
+    <header>
+      <h3>Project</h3>
+    </header>
+    <section>
+      <p>Description</p>
+    </section>
+  </article>
+);
+
+export default ProjectCard;
