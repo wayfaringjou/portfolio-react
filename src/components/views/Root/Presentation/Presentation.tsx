745e63eae787048254d5ae40df9fd9f98227a248
Joel Del Cueto <wayfaringjou@gmail.com>
2021-04-25 15:54:42 -0400

    Add exploded css style and move cover to it's own partial

--- src/components/views/Root/Presentation/Presentation.tsx
+++ src/components/views/Root/Presentation/Presentation.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable max-len */
 import React, { ReactElement, useEffect, useState } from 'react';
 import fetchGraphQL from '../../../../utils/fetchGraphQL';
-import backdrop from '../../../../img/layered-schema/backdrop.svg';
-import layout from '../../../../img/layered-schema/layout.svg';
-import content from '../../../../img/layered-schema/content.svg';
+// import backdrop from '../../../../img/layered-schema/backdrop.svg';
+// import layout from '../../../../img/layered-schema/layout.svg';
+// import content from '../../../../img/layered-schema/content.svg';
+
+import background from '../../../../img/illustration/1_Background.svg';
+import backgroundOutline from '../../../../img/illustration/2_Background - outline.svg';
+import layout from '../../../../img/illustration/3_Layout.svg';
+import layoutOutline from '../../../../img/illustration/4_Layout - outline.svg';
+import content from '../../../../img/illustration/5_content.svg';
+import contentOutline from '../../../../img/illustration/6_content - outline.svg';
 
 /*
 {
@@ -45,10 +53,15 @@ const Presentation = (): ReactElement => {
 
   return (
     <>
-      <section className="cover bg-5">
+      <section className="cover bg-2">
         <div className="with-adjacent centered">
-          <div className="bg-1">
-            <img src={backdrop} alt="img" />
+          <div className="exploded">
+            <img src={background} alt="img" />
+            <img src={backgroundOutline} alt="img" />
+            <img src={layout} alt="img" />
+            <img src={layoutOutline} alt="img" />
+            <img src={content} alt="img" />
+            <img src={contentOutline} alt="img" />
           </div>
           <div className="">
             <h1 className="">Let&apos;s build something great together!</h1>
