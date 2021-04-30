11dcde598aa28f0d9e22683db8d3655c1d1e52c0
Joel Del Cueto <wayfaringjou@gmail.com>
2021-04-30 13:49:30 -0400

    Overhaul typescale formula

--- src/components/views/Root/About/About.tsx
+++ src/components/views/Root/About/About.tsx
@@ -1,5 +1,11 @@
 import React, { ReactElement } from 'react';
 
-const About = (): ReactElement => <h6>About</h6>;
+const About = (): ReactElement => (
+  <>
+    <h6>About</h6>
+    <div className="overline">Text</div>
+    <div className="caption">Text</div>
+  </>
+);
 
 export default About;
