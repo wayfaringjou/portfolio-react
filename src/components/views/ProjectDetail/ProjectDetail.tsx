75ee5d01d39235ca649e41bc2172df66d502feb6
Joel Del Cueto <wayfaringjou@gmail.com>
2021-05-23 21:20:26 -0400

    Add ProjectDetail and ProjectShowcase components

---
+++ src/components/views/ProjectDetail/ProjectDetail.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const ProjectDetail = (): React.ReactElement => (
+  <section>
+    <h2>Project Detail</h2>
+  </section>
+);
+
+export default ProjectDetail;
---
+++ src/components/views/Root/WorkOverview/ProjectShowcase.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const ProjectShowcase = (): React.ReactElement => (
+  <section>
+    <h2>Project Detail</h2>
+  </section>
+);
+
+export default ProjectShowcase;
